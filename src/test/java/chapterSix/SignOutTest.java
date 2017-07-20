1843e02f8822400ed536b41073afb7c27a03a04f
Dushicici <cinthya.poulina@polteq.com>
2017-07-20 14:57:25 +0200

    Init commit

---
+++ src/test/java/chapterSix/SignOutTest.java
@@ -0,0 +1,44 @@
+package chapterSix;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Cinthya on 20-7-2017.
+ */
+
+public class SignOutTest {
+
+    @Test
+    public void logOut (){
+        ChromeDriverManager.getInstance().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://techblog.polteq.com/testshop/index.php");
+
+        driver.findElement(By.className("login")).click();
+        driver.findElement(By.id("email")).sendKeys("tester@test.com");
+        driver.findElement(By.id("passwd")).sendKeys("1qazxsw2");
+        driver.findElement(By.id("SubmitLogin")).click();
+        WebElement el = driver.findElement(By.className("info-account"));
+
+        el.getText();
+        Assertions.assertThat(el.getText()).as("welcome text").isEqualTo
+            ("Welcome to your account. Here you can manage all of your personal information and orders.");
+
+        driver.findElement(By.className("logout")).click();
+        WebElement lo = driver.findElement(By.className("page-heading"));
+
+        lo.getText();
+        Assertions.assertThat(lo.getText()).as("autenticatietekst").isEqualTo
+            ("AUTHENTICATION");
+
+        driver.quit();
+}
+}
