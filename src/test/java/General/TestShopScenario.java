fdbef2de63600cd68784dde4593bb21749f9123f
Dushicici <cinthya.poulina@polteq.com>
2017-07-22 20:33:31 +0200

    Added driver to the TestShopScenario

--- src/test/java/General/TestShopScenario.java
+++ src/test/java/General/TestShopScenario.java
@@ -3,6 +3,7 @@ package General;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,11 +14,15 @@ import org.testng.annotations.Test;
 public class TestShopScenario {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setUp(){
+
+
         ChromeDriverManager.getInstance().setup();
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
 
         //open the website
         driver.manage().window().maximize();
