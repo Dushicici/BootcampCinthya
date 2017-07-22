e070db5829d8dc288110ba0e085a0439fbf0265d
Dushicici <cinthya.poulina@polteq.com>
2017-07-22 20:29:56 +0200

    Changed SignOutTest

---
+++ src/main/java/pages/ContactUsPage.java
@@ -0,0 +1,50 @@
+package pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * Created by Cinthya on 21-7-2017.
+ */
+public class ContactUsPage {
+
+    private WebDriver driver;
+
+    @FindBy(css = "#email")
+    private WebElement emailTextField;
+
+    @FindBy(css = "#id_order")
+    private WebElement orderReferenceTextField;
+
+    @FindBy(css = "#message")
+    private WebElement messageTextField;
+
+    @FindBy(css = "button#submitMessage")
+    private WebElement sendButton;
+
+    @FindBy(xpath = ".//p[@class='alert alert-success']")
+    private WebElement succesMessage;
+
+    public ContactUsPage (WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void fillInContactForm(String email, String orderReference, String message){
+        Select subjectHeading = new Select (driver.findElement(By.cssSelector("select#id_contact")));
+        subjectHeading.selectByVisibleText("Customer service");
+
+        emailTextField.sendKeys(email);
+        orderReferenceTextField.sendKeys(orderReference);
+        messageTextField.sendKeys(message);
+        sendButton.click();
+    }
+    public String getAlertMessage(){
+        return succesMessage.getText();
+    }
+}
